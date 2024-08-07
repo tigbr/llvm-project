e527f8442b1f975af740e96efe63139b980768b9
Gábor Tóthvári <tigbrcode@protonmail.com>
2024-08-07 18:59:56 +0200

    Add test cases.

--- clang-tools-extra/test/clang-tidy/checkers/misc/convert-methods-to-global-functions.cpp
+++ clang-tools-extra/test/clang-tidy/checkers/misc/convert-methods-to-global-functions.cpp
@@ -1,14 +1,59 @@
 // RUN: %check_clang_tidy %s misc-convert-methods-to-global-functions %t
 
-// FIXME: Add something that triggers the check here.
-void f();
-// CHECK-MESSAGES: :[[@LINE-1]]:6: warning: function 'f' is insufficiently awesome [misc-convert-methods-to-global-functions]
-
 // FIXME: Verify the applied fix.
 //   * Make the CHECK patterns specific enough and try to make verified lines
 //     unique to avoid incorrect matches.
 //   * Use {{}} for regular expressions.
 // CHECK-FIXES: {{^}}void awesome_f();{{$}}
 
-// FIXME: Add something that doesn't trigger the check here.
-void awesome_f2();
+#include <vector>
+
+struct NoStd {
+	int a;
+
+	void begin() {
+
+	}
+};
+
+struct Foo {
+ 	std::vector<int> numbers;
+	auto getNumbers(){
+		return numbers;
+	}
+};
+
+#define concat(a,b,c) a.b##c()
+
+int main(void) {
+
+	NoStd asdf;
+	asdf.begin();
+
+	std::vector<int> v;
+	auto a = v.begin();
+	auto b = v.end();
+	bool c = v.empty();
+	std::size_t d = v.size();
+
+	for (auto i : v) {
+		
+	} 
+
+	std::vector<int> size;
+	for (auto i : size) {
+		
+	} 
+
+	concat(size, be, gin);
+
+	struct Foo f;
+	auto g  = f.numbers.size();
+	auto g2 = f.getNumbers().size();
+
+    auto x = &v;
+    x->end();
+	
+	return 0;
+}
+
