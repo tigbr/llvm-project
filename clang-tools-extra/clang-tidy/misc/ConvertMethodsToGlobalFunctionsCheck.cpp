af1e46887b145f12825ad305d190a9a64040785d
Botond Istvan Horvath <horvath.botond.istvan@gmail.com>
2024-08-15 13:25:27 +0200

    Added checks for const, paramcount and stopped a::begin() from beeng rewrited

--- clang-tools-extra/clang-tidy/misc/ConvertMethodsToGlobalFunctionsCheck.cpp
+++ clang-tools-extra/clang-tidy/misc/ConvertMethodsToGlobalFunctionsCheck.cpp
@@ -8,6 +8,7 @@
 
 #include "ConvertMethodsToGlobalFunctionsCheck.h"
 #include "clang/ASTMatchers/ASTMatchFinder.h"
+#include "clang/ASTMatchers/ASTMatchers.h"
 #include "clang/Lex/Lexer.h"
 #include "llvm/Support/raw_ostream.h"
 
@@ -15,56 +16,106 @@ using namespace clang::ast_matchers;
 
 namespace clang::tidy::misc {
 
-void ConvertMethodsToGlobalFunctionsCheck::registerMatchers(MatchFinder *Finder) {
-  Finder->addMatcher(cxxMemberCallExpr(callee(cxxMethodDecl(hasAnyName("begin","end","size","empty","cbegin","cned","rbegin","rend","crbegin","crend")))).bind("root"), this);
+void ConvertMethodsToGlobalFunctionsCheck::registerMatchers(
+    MatchFinder *Finder) {
+  Finder->addMatcher(
+      cxxMemberCallExpr(
+          callee(cxxMethodDecl(hasAnyName("cbegin", "cend", "crbegin", "crend"),
+                               isConst(), parameterCountIs(0))))
+          .bind("root"),
+      this);
+  Finder->addMatcher(
+      cxxMemberCallExpr(
+          callee(cxxMethodDecl(hasAnyName("begin", "end", "rbegin", "rend"),
+                               parameterCountIs(0))))
+          .bind("root"),
+      this);
+  // Finder->addMatcher(cxxMemberCallExpr(callee(cxxMethodDecl(hasAnyName("swap"),parameterCountIs(1)))).bind("root"),
+  // this);
+  if (getLangOpts().CPlusPlus17)
+    Finder->addMatcher(
+        cxxMemberCallExpr(callee(cxxMethodDecl(hasAnyName("size", "empty"),
+                                               isConst(), parameterCountIs(0))))
+            .bind("root"),
+        this);
 }
 
-bool ConvertMethodsToGlobalFunctionsCheck::isLanguageVersionSupported(const LangOptions &LangOpts) const {
-	return LangOpts.CPlusPlus11;
+bool ConvertMethodsToGlobalFunctionsCheck::isLanguageVersionSupported(
+    const LangOptions &LangOpts) const {
+  return LangOpts.CPlusPlus11;
 }
 
-void ConvertMethodsToGlobalFunctionsCheck::check(const MatchFinder::MatchResult &Result) {
+void ConvertMethodsToGlobalFunctionsCheck::check(
+    const MatchFinder::MatchResult &Result) {
 
-  const auto *MemberCallExpr = Result.Nodes.getNodeAs<CXXMemberCallExpr>("root");
-  const auto *MethodDecl = llvm::dyn_cast_or_null<CXXMethodDecl>(MemberCallExpr->getDirectCallee());
+  const auto *MemberCallExpr =
+      Result.Nodes.getNodeAs<CXXMemberCallExpr>("root");
+  const auto *MethodDecl =
+      llvm::dyn_cast_or_null<CXXMethodDecl>(MemberCallExpr->getDirectCallee());
   const auto *ObjectDecl = MethodDecl->getParent();
-  assert(MemberCallExpr && MethodDecl && ObjectDecl && "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+  assert(MemberCallExpr && MethodDecl && ObjectDecl &&
+         "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
 
-  if (!ObjectDecl->isInStdNamespace()) return;
+  // if (!ObjectDecl->isInStdNamespace()) return;
 
   SourceLocation begin{MemberCallExpr->getBeginLoc()};
   SourceLocation end{MemberCallExpr->getRParenLoc()};
-	if (begin.isInvalid() || end.isInvalid() || begin.isMacroID() || end.isMacroID()) return;
-
+  if (begin.isInvalid() || end.isInvalid() || begin.isMacroID() ||
+      end.isMacroID())
+    return;
   std::string fixit;
-
   clang::LangOptions lopt = getLangOpts();
 
-  if (clang::Lexer::getSourceText({{begin,end},true}, *Result.SourceManager, lopt)==":") return;
+  if (clang::Lexer::getSourceText({{begin, end}, true}, *Result.SourceManager,
+                                  lopt) == ":")
+    return;
 
   if (begin == MemberCallExpr->getExprLoc()) {
-    fixit = "std::" + std::string{MethodDecl->getName().data()} + "(*this)";
+    fixit = "std::" + std::string{MethodDecl->getName().data()} + "(*this";
+    for (size_t i = 0; i < MemberCallExpr->getNumArgs(); i++) {
+      fixit += ", ";
+      fixit += clang::Lexer::getSourceText(
+          {MemberCallExpr->getArg(i)->getSourceRange(), true},
+          *Result.SourceManager, lopt);
+    }
+    fixit += ")";
   } else {
-	SourceLocation currentLocation;
-	SourceLocation tokenLocation = begin;
+    SourceLocation currentLocation;
+    SourceLocation tokenLocation = begin;
     std::optional<Token> prevtoken;
     std::optional<Token> token;
-	do {
-		currentLocation = tokenLocation;
-		prevtoken = token;
-		token = clang::Lexer::findNextToken(currentLocation, *Result.SourceManager, lopt);
-		tokenLocation = token->getLocation();
-	} while (tokenLocation != MemberCallExpr->getExprLoc());
+    do {
+      currentLocation = tokenLocation;
+      prevtoken = token;
+      token = clang::Lexer::findNextToken(currentLocation,
+                                          *Result.SourceManager, lopt);
+      tokenLocation = token->getLocation();
+    } while (tokenLocation != MemberCallExpr->getExprLoc());
     CharSourceRange sourceRange{{begin, prevtoken->getLocation()}, true};
-	StringRef textUntilDot = clang::Lexer::getSourceText(sourceRange, *Result.SourceManager, lopt);
-    std::string tokenAsString = clang::Lexer::getSpelling(*prevtoken, *Result.SourceManager, lopt);
-	if (tokenAsString == "->*" || tokenAsString == ".*") return;
-    fixit = "std::" + std::string{MethodDecl->getName().data()} + std::string{"("} + (prevtoken->getLength() == 2 ? std::string{"*"} : std::string{""}) + std::string{textUntilDot.drop_back(prevtoken->getLength())} + std::string{")"};
-    
+    StringRef textUntilDot =
+        clang::Lexer::getSourceText(sourceRange, *Result.SourceManager, lopt);
+    std::string tokenAsString =
+        clang::Lexer::getSpelling(*prevtoken, *Result.SourceManager, lopt);
+
+    if (!(tokenAsString == "->" || tokenAsString == "."))
+      return;
+    fixit = "std::" + std::string{MethodDecl->getName().data()} +
+            std::string{"("} +
+            (tokenAsString == "->" ? std::string{"*"} : std::string{""}) +
+            std::string{textUntilDot.drop_back(prevtoken->getLength())};
+    for (size_t i = 0; i < MemberCallExpr->getNumArgs(); i++) {
+      fixit += ", ";
+      fixit += clang::Lexer::getSourceText(
+          {MemberCallExpr->getArg(i)->getSourceRange(), true},
+          *Result.SourceManager, lopt);
+    }
+    fixit += ")";
   }
 
-    diag(MemberCallExpr->getRParenLoc(), "is not using the global version")
-    << FixItHint::CreateReplacement({MemberCallExpr->getBeginLoc(), MemberCallExpr->getRParenLoc()}, fixit);
+  diag(MemberCallExpr->getRParenLoc(), "is not using the global version")
+      << FixItHint::CreateReplacement(
+             {MemberCallExpr->getBeginLoc(), MemberCallExpr->getRParenLoc()},
+             fixit);
 }
 
 } // namespace clang::tidy::misc
--- clang-tools-extra/test/clang-tidy/checkers/misc/convert-methods-to-global-functions.cpp
+++ clang-tools-extra/test/clang-tidy/checkers/misc/convert-methods-to-global-functions.cpp
@@ -1,12 +1,30 @@
-// RUN: %check_clang_tidy %s misc-convert-methods-to-global-functions %t
+// RUN: %check_clang_tidy %s misc-convert-methods-to-global-functions %t -std=c++17
 
 // FIXME: Verify the applied fix.
 //   * Make the CHECK patterns specific enough and try to make verified lines
 //     unique to avoid incorrect matches.
 //   * Use {{}} for regular expressions.
-// CHECK-FIXES: {{^}}void awesome_f();{{$}}
+// C H E C K-FIXES: {{^}}void awesome_f();{{$}}
 
-#include <vector>
+namespace std {
+  typedef unsigned long long size_t;
+  template<class T>
+  struct vector{
+    T* begin(){return nullptr;}
+    T* end(){return nullptr;}
+    bool empty() const{return true;}
+    size_t size() const{return 0;}
+    const T* cbegin()const{return nullptr;}
+    const T* crbegin()const{return nullptr;}
+    T* rbegin(){return nullptr;}
+    T* rend(){return nullptr;}
+    const T* cend()const{return nullptr;}
+    const T* crend()const{return nullptr;}
+    void swap(vector<T>&o){};
+  };
+  template <class T>
+  void swap(T&,T&){}
+}
 
 struct NoStd {
 	int a;
@@ -22,6 +40,11 @@ struct Foo {
 		return numbers;
 	}
 };
+struct D:std::vector<int>{
+  std::size_t foo(){
+    return std::vector<int>::size();
+  }
+};
 
 #define concat(a,b,c) a.b##c()
 
@@ -29,12 +52,25 @@ int main(void) {
 
 	NoStd asdf;
 	asdf.begin();
+  // CHECK-MESSAGES: [[@LINE-1]]:13: warning: is not using the global version [misc-convert-methods-to-global-functions]
+  // CHECK-FIXES: std::begin(asdf)
 
-	std::vector<int> v;
+	std::vector<int> v,v2;
+//  v.swap(v2);
+  // C HECK-MESSAGES: [[@LINE-1]]:12: warning: is not using the global version [misc-convert-methods-to-global-functions]
+  // C HECK-FIXES: std::swap(v, v2)
 	auto a = v.begin();
+  // CHECK-MESSAGES: [[@LINE-1]]:19: warning: is not using the global version [misc-convert-methods-to-global-functions]
+  // CHECK-FIXES: std::begin(v)
 	auto b = v.end();
+  // CHECK-MESSAGES: [[@LINE-1]]:17: warning: is not using the global version [misc-convert-methods-to-global-functions]
+  // CHECK-FIXES: std::end(v)
 	bool c = v.empty();
+  // CHECK-MESSAGES: [[@LINE-1]]:19: warning: is not using the global version [misc-convert-methods-to-global-functions]
+  // CHECK-FIXES: std::empty(v)
 	std::size_t d = v.size();
+  // CHECK-MESSAGES: [[@LINE-1]]:25: warning: is not using the global version [misc-convert-methods-to-global-functions]
+  // CHECK-FIXES: std::size(v)
 
 	for (auto i : v) {
 		
@@ -49,11 +85,41 @@ int main(void) {
 
 	struct Foo f;
 	auto g  = f.numbers.size();
+  // CHECK-MESSAGES: [[@LINE-1]]:27: warning: is not using the global version [misc-convert-methods-to-global-functions]
+  // CHECK-FIXES: std::size(f.numbers)
 	auto g2 = f.getNumbers().size();
+  // CHECK-MESSAGES: [[@LINE-1]]:32: warning: is not using the global version [misc-convert-methods-to-global-functions]
+  // CHECK-FIXES: std::size(f.getNumbers())
 
-    auto x = &v;
-    x->end();
+  auto x = &v;
+  x->end();
+  // CHECK-MESSAGES: [[@LINE-1]]:10: warning: is not using the global version [misc-convert-methods-to-global-functions]
+  // CHECK-FIXES: std::end(*x)
+  std::vector<std::vector<int>> vec;
+
+  for (int x: *vec.begin()){
+    // CHECK-MESSAGES: [[@LINE-1]]:26: warning: is not using the global version [misc-convert-methods-to-global-functions]
+    // CHECK-FIXES: std::begin(vec)
+    vec.cbegin();
+    // CHECK-MESSAGES: [[@LINE-1]]:16: warning: is not using the global version [misc-convert-methods-to-global-functions]
+    // CHECK-FIXES: std::cbegin(vec)
+  }
+  vec.crend();
+  // CHECK-MESSAGES: [[@LINE-1]]:13: warning: is not using the global version [misc-convert-methods-to-global-functions]
+  // CHECK-FIXES: std::crend(vec)
+  vec.rend();
+  // CHECK-MESSAGES: [[@LINE-1]]:12: warning: is not using the global version [misc-convert-methods-to-global-functions]
+  // CHECK-FIXES: std::rend(vec)
+  vec.rbegin();
+  // CHECK-MESSAGES: [[@LINE-1]]:14: warning: is not using the global version [misc-convert-methods-to-global-functions]
+  // CHECK-FIXES: std::rbegin(vec)
+  vec.crbegin();
+  // CHECK-MESSAGES: [[@LINE-1]]:15: warning: is not using the global version [misc-convert-methods-to-global-functions]
+  // CHECK-FIXES: std::crbegin(vec)
 	
+  vec.cend();
+  // CHECK-MESSAGES: [[@LINE-1]]:12: warning: is not using the global version [misc-convert-methods-to-global-functions]
+  // CHECK-FIXES: std::cend(vec)
 	return 0;
 }
 
