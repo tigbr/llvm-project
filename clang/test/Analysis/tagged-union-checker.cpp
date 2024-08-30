dca07042fe0bd9519dbad478212203c7de71ec8b
Gábor Tóthvári <tigbrcode@protonmail.com>
2024-08-30 10:26:55 +0200

    haladas

--- clang/test/Analysis/tagged-union-checker.cpp
+++ clang/test/Analysis/tagged-union-checker.cpp
@@ -1,36 +1,52 @@
 // RUN: %clang_analyze_cc1 -analyzer-checker=core,debug.ExprInspection -fblocks -verify -analyzer-config eagerly-assume=false %s
 
 enum tag_values {
-	tag1,
-	tag2,
-	tag3,
+	t1,
+	t2,
+	t3,
 };
 
 struct bar {
 	enum tag_values type;
 	union {
-		char c;
-		short s;
-		int i;
-		long l;
+		char d1;
+		short d2;
+		int d3;
+		long d4;
 	} data;
 };
 
 void foo(tag_values tag) {
     // expected-warning@+1 {{"Inconsistent tagged union access!"}}
     switch (tag) {
-		case tag1: break;
-		case tag2: break;
-		case tag3: break;
+		case t1: break;
+		case t2: break;
+		case t3: break;
 	}
 }
 
 int main(int argc, char **argv) {
 	struct bar a;
 
-	a.type = tag3;
-	a.data.s = 43;
+	// Ezt hogyan lenne jó kezelni?
+	// Elvileg tetszőleges sorrendben is be lehet állítani az unió és az enum értékét.
+	// a.data.d1 = 1;
+	// a.type = t1;
 
-	a.data.s = 44;
+	a.type = t1;
+	a.data.d1 = 1;
+
+	a.type = t2;
+	a.data.d2 = 1;
+
+	a.data.d3 = 1;
+
+	switch (a.type) {
+		case t1: a.data.d1 = 1; break;
+		case t2: a.data.d2 = 1; break;
+		case t3: a.data.d3 = 1; break;
+	}
+
 	return 0;
 }
+
